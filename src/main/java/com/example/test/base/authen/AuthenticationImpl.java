5a5c3dec7b75a2b05b666260533ce72f15a311f0
vongsaly <toto.vse2@gmail.com>
2026-03-07 09:53:27 +0700

    feat: build base with authentication, token helper in filter chain

---
+++ src/main/java/com/example/test/base/authen/AuthenticationImpl.java
@@ -0,0 +1,41 @@
+package com.example.test.base.authen;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import com.example.test.base.authen.filter_chain.RequestContext;
+import com.example.test.base.authen.filter_chain.TokenFilterChain;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Aspect
+@Component
+public class AuthenticationImpl {
+    private TokenFilterChain tokenFilterChain;
+    public AuthenticationImpl(@Qualifier("start-filter-chain") TokenFilterChain tokenFilterChain) {
+        this.tokenFilterChain = tokenFilterChain;
+    }
+
+    @Before("@annotation(com.example.test.base.authen.AuthenticationProxy)")
+    public void validate(JoinPoint joinPoint) {
+        String accessToken = getAccessTokenForParams(joinPoint);
+        List<String> rolesAcceptForApi = getRolesAcceptFromParams(joinPoint);
+        RequestContext requestContext = new RequestContext(accessToken, rolesAcceptForApi);
+
+        tokenFilterChain.validate(requestContext);
+    }
+
+    private String getAccessTokenForParams(JoinPoint joinPoint){
+        return (String) joinPoint.getArgs()[0];
+    }
+
+    private List<String> getRolesAcceptFromParams(JoinPoint joinPoint){
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String[] values = signature.getMethod().getAnnotation(AuthenticationProxy.class).acceptRoles();
+        return List.of(values);
+    }
+}
