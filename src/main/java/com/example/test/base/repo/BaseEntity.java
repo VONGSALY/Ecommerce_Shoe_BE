13d176b4e51511a2bc5aeeedf231c02c3a19ddbb
vongsaly <toto.vse2@gmail.com>
2026-03-08 08:20:07 +0700

    feat: build base with repository common

---
+++ src/main/java/com/example/test/base/repo/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.example.test.base.repo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public abstract class BaseEntity {
+}
---
+++ src/main/java/com/example/test/base/repo/BaseRepository.java
@@ -0,0 +1,8 @@
+package com.example.test.base.repo;//package fa.training.HN24_CPL_JAVA_01_G3.base;
+//
+//import lombok.AllArgsConstructor;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
+//}
