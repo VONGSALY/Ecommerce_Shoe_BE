c9261c2339c0b8cf6eaf0fd7a670d03ac0e4d23c
vongsaly <toto.vse2@gmail.com>
2026-03-08 15:20:51 +0700

    feat: add role enums in account

---
+++ src/main/java/com/example/test/common/enums/RoleEnums.java
@@ -0,0 +1,10 @@
+package com.example.test.common.enums;
+
+public enum RoleEnums {
+    EMPLOYEE("EMPLOYEE"),
+    CUSTOMER("CUSTOMER"),
+    ADMIN("ADMIN");
+
+    RoleEnums(String manager) {
+    }
+}
