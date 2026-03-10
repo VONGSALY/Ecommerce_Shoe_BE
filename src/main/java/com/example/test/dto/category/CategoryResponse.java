ebfb9d2ee990705bb648e56197669da83f7f2ac1
vongsaly <toto.vse2@gmail.com>
2026-03-10 23:11:00 +0700

    feat: add dto category response

---
+++ src/main/java/com/example/test/dto/category/CategoryResponse.java
@@ -0,0 +1,19 @@
+package com.example.test.dto.category;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class CategoryResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate createDate;
+    private LocalDate modifyDate;
+    private Boolean isActive;
+}
